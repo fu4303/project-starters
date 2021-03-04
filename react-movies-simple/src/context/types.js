eec5318c527d689a9eafaa4826b3dc7c800d8ef4
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-03-04 15:47:56 -0500

    Add files via upload

---
+++ react-movies-simple/src/context/types.js
@@ -0,0 +1,7 @@
+export const SEARCH_MOVIES = "SEARCH_MOVIES";
+export const GET_MOVIE = "GET_MOVIE";
+export const CLEAR_MOVIES = "CLEAR_MOVIES";
+export const GET_REVIEWS = "GET_REVIEWS";
+export const SET_LOADING = "SET_LOADING";
+export const REMOVE_ALERT = "REMOVE_ALERT";
+export const SET_ALERT = "SET_ALERT";
