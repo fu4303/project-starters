8a4c854e19925eae79f93f195f5665445f532868
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-04-11 09:46:53 -0400

    Add files via upload

---
+++ shakespeare-translator/scripts/app.js
@@ -0,0 +1,28 @@
+// read input
+let inputElement = document.querySelector("#inputTxt");
+
+// button click
+let translateBtnElement = document.querySelector("#translateButton");
+
+// show output
+let outputElement = document.querySelector("#outputTxt");
+
+let url="https://shakespeare.p.mashape.com/shakespeare.json";
+
+function translatedURL(inputValue){
+    var encodedURI = encodeURI(inputValue);
+    let translatedURL = `${url}?text=${encodedURI}`;
+    return translatedURL;   
+}
+
+function translateFunction(event){
+    let inputValue = inputElement.value;    
+    let finalURL = translatedURL(inputValue);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            outputElement.innerText = json.contents.translated;
+        })
+        .catch(() => alert("Shakespeare(Server) is busy! Try after sometime"))
+}
+translateBtnElement.addEventListener("click", translateFunction);
